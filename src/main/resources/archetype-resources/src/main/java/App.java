d9259fe38c169de3bb29d48203d44753dfe4e487
EdgySantana <tesseus2006@gmail.com>
2022-10-03 14:55:32 -0400

    Simple pom.xml file using maven

---
+++ src/main/resources/archetype-resources/src/main/java/App.java
@@ -0,0 +1,13 @@
+package $org.plantplaces;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
